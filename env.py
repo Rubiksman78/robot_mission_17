f17a68ac9ba0bc09c72c3315043fce61a8c8a168
Rubiksman78 <samuelabc78310@gmail.com>
2025-03-21 15:02:38 +0100

    env,obs+step

--- env.py
+++ env.py
@@ -1,6 +1,5 @@
 import numpy as np
-from typing import List, Tuple
-from mesa import Agent, Model
+from mesa import Agent
 from mesa.space import MultiGrid
 from agents import RobotAgent
 
@@ -26,30 +25,46 @@ class Radioactivity(Agent):
         self.is_wall = 0
 
 class Environment:
-    def __init__(self, grid:MultiGrid, agent_coords:List[Tuple[int, int]]):
+    def __init__(self, grid:MultiGrid):
         self.grid = grid
-        self.agent_coords = agent_coords
 
-
-    def step(self, pos, action):
+    def step(self, agent, action):
         #called in model.do
-        #find way to get pos from Model
-        if action == 0:
-            agent = self.grid.get_cell_list_contents(pos)[0]
-            agent.color_waste = 0
-        #check success
-        success = False
+        #desired output is [radioactivity_level 3*3, color_waste 3*3, is_waste_disposal 3*3, is_wall 3*3, other_robots 3*3, success]
+        pos = agent.pos
+        cell_agent = self.grid.get_cell_list_contents(pos)[0] #get agents at the same pos as the robot (Waste+Radioactivity)
+        #Get information from neighbouts
+        radioactivity_level = np.zeros((3, 3))
+        color_waste = np.zeros((3, 3))
+        is_waste_disposal = np.zeros((3, 3))
+        is_wall = np.zeros((3, 3))
+        other_robots = np.zeros((3, 3))
         neighbours = self.grid.get_neighborhood(pos, moore=True, include_center=False)
-        neighbours_info = neighbours
-        return success + neighbours_info
+        success = self.can_pickup(pos)
+        for i, neighbour in enumerate(neighbours):
+            neighbour_agent = self.grid.get_cell_list_contents(neighbour)[0]
+            x,y = i//3, i%3
+            radioactivity_level[x][y] = neighbour_agent.radioactivity_level
+            color_waste[x][y] = neighbour_agent.color_waste
+            is_waste_disposal[x][y] = neighbour_agent.is_waste_disposal
+            is_wall[x][y] = neighbour_agent.is_wall
+            if isinstance(neighbour_agent, RobotAgent):
+                other_robots[x][y] = 1
+        observation = [radioactivity_level, color_waste, is_waste_disposal, is_wall, other_robots, success]
+        #Update the cell according to the action
+        if action in [0, 1, 2, 3] and success:
+            cell_agent.color_waste = action
+        elif action in [4, 5, 6, 7]:
+            new_position = neighbours[action-4]
+            self.grid.move_agent(cell_agent, new_position)
+        return observation
     
     def reset(self):
         pass
     
     def can_pickup(self, pos):
-        #check if there is not another robot heree
+        #check if there is not another robot here
         cell_agent = self.grid.get_cell_list_contents(pos)
-        #check if it is a RobotAgent
         if len(cell_agent) == 1 and isinstance(cell_agent[0], RobotAgent):
             return False
         return True
